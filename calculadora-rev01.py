83bd801d663f7f6b1881ddab7bd84be6d5d5a621
danilocruzcastro <cruz.castro.ofc@gmail.com>
2026-03-10 08:47:55 -0300

    Create calculadora-rev01.py

---
+++ calculadora-rev01.py
@@ -0,0 +1,12 @@
+num1 = float(input("Primeiro número: "))
+num2 = float(input("Segundo número: "))
+
+print("1 - Somar")
+print("2 - Subtrair")
+
+op = input("Escolha: ")
+
+if op == "1": 
+      print("Resultado:", num1 + num2)
+elif op == "2":
+      PRINT("Resultado:", num1 - num2)
